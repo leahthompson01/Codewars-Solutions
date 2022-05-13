04db54295d150a3504a9f8caf953572faf924d97
Leah Thompson <97814431+leahthompson01@users.noreply.github.com>
2022-05-13 06:03:04 -0400

    new codewars solution

---
+++ Even|Odd|Neither.js
@@ -0,0 +1,12 @@
+function oddOrEven(n) {
+  //n = 8 1+2+3+4+5+6+7+8 
+  if(n % 2 === 0){
+    if(((n/2) % 2 !== 0)){
+       return ODD
+       }else {
+      return EVEN
+    }
+  }
+  
+return EITHER ;
+}
