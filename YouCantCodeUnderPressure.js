6269f6af741ee573e44cffa1b9650d10f58778ed
leahthompson01 <leahmthompson03@gmail.com>
2022-05-31 22:51:43 -0400

    added description and link for problem

--- YouCantCodeUnderPressure.js
+++ YouCantCodeUnderPressure.js
@@ -1,3 +1,6 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/javascript
+// Code as fast as you can! You need to double the integer and return it.
+
 function doubleInteger(i) {
   
     return i*2;
